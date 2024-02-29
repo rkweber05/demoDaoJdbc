fbadc342e0ea015e4beea2531a200a449111ea30
rkweber05 <rkweber05@outlook.com>
2024-02-29 14:02:56 -0300

    Seller entity class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,11 +1,17 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
 
-        Department department = new Department(1, "Books");
-        System.out.println(department);
+        Department obj = new Department(1, "Books");
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+
+        System.out.println(seller);
     }
 }
