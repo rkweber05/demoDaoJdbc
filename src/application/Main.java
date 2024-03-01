e271302e88de84dc72311ae5d78a42d588cd109c
rkweber05 <rkweber05@outlook.com>
2024-03-01 07:55:45 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,6 +13,8 @@ public class Main {
         Department obj = new Department(1, "Books");
 
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao(); //não conhece a implementação e sim a interface
 
         System.out.println(seller);
     }
